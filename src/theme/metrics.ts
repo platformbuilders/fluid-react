bb4540b1bb61ca248e470056aea03fcd4835c606
Luiz Fernando <luizepauloxd@gmail.com>
2020-04-14 15:44:49 -0300

    fix(ts): fixes typescript compiling error

--- src/theme/metrics.ts
+++ src/theme/metrics.ts
@@ -7,7 +7,7 @@ export const scale = (size: number): number =>
   (window.innerWidth / guidelineBaseWidth) * size;
 export const verticalScale = (size: number): number =>
   (window.innerHeight / guidelineBaseHeight) * size;
-export const moderateScale = (size: number, factor = 0.5) =>
+export const moderateScale = (size: number, factor = 0.5): number =>
   size + (scale(size) - size) * factor;
 
 export default {
