647593f041f6cad0aebfa834ad5622d877d3f8d3
Marcelo Silva <maarcelomonteiro@hotmail.com>
2020-05-07 15:26:48 -0300

    chore: configured storybook base

---
+++ storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-backgrounds/register',
+    'storybook-addon-paddings',
+    'storybook-addon-jsx/register',
+    'storybook-readme/register',
+  ],
+};
