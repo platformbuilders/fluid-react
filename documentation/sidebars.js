289b104cd929158bb919de4581816bacbddeec82
Gabao-Farias <gabrielfariasbass@gmail.com>
2020-11-23 09:47:46 -0300

    feat(documentation): added native component avatar doc

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -1,37 +1,42 @@
 module.exports = {
   someSidebar: {
-    Começando: ['introduction.introduction'],
-    Componentes: [
-      {
-        Avatar: [
-          'component.avatar',
-          {
-            Atributos: [
-              'component.avatar.attributes.alt',
-              'component.avatar.attributes.children',
-              'component.avatar.attributes.key',
-              'component.avatar.attributes.onPress',
-              'component.avatar.attributes.src',
-              'component.avatar.attributes.variant',
-            ],
-          },
-        ],
-      },
-      'component.button',
-      'component.checkBox',
-      'component.formContainer',
-      'component.formError',
-      'component.icon',
-      'component.loadingIndicator',
-      'component.paper',
-      'component.Select',
-      'component.textInput',
-      {
-        Typography: [
-          'component.typography',
-          'component.typography.styleGuide'
-        ]
-      }
-    ],
-  },
+    Web: [{
+      Começando: ['introduction.introduction'],
+      Componentes: [
+        {
+          Avatar: [
+            'component.avatar',
+            {
+              Atributos: [
+                'component.avatar.attributes.alt',
+                'component.avatar.attributes.children',
+                'component.avatar.attributes.key',
+                'component.avatar.attributes.onPress',
+                'component.avatar.attributes.src',
+                'component.avatar.attributes.variant',
+              ],
+            },
+          ],
+        },
+        'component.button',
+        'component.checkBox',
+        'component.formContainer',
+        'component.formError',
+        'component.icon',
+        'component.loadingIndicator',
+        'component.paper',
+        'component.Select',
+        'component.textInput',
+        {
+          Typography: [
+            'component.typography',
+            'component.typography.styleGuide'
+          ]
+        }
+      ],
+    }],
+    Native: [
+      'native.component.avatar'
+    ]
+  }  
 };
