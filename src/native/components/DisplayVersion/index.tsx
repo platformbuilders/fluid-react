230de7b540aa7da01d8dd890b0cb82b7d78c1edc
Glauber Brack <57924169+glauberbrack@users.noreply.github.com>
2021-05-03 09:34:25 -0300

    feat: create react native display version component (#66)
    * Update package.json
    
    * Update package.json
    
    * feat: create react native display version component
    
    * fix: package version and use spacing from theme
    
    Co-authored-by: Eduardo Spada <duspada@users.noreply.github.com>

---
+++ src/native/components/DisplayVersion/index.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { FC } from 'react';
+import { Text } from './styles';
+
+type Props = {
+  appVersion?: string;
+  buildVersion?: string;
+};
+
+const DisplayVersion: FC<Props> = ({ appVersion, buildVersion }) => (
+  <Text accessibility="display-version">
+    `Versão ({appVersion}/{buildVersion}
+    )`
+  </Text>
+);
+
+export default DisplayVersion;
