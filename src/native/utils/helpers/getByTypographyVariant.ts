e6fe364dd6011aabff938c418f0cd2ac07c1aac9
Michael Amaral <michael.amaral@platformbuilders.io>
2021-05-18 02:38:41 -0300

    fix(undefined-variant): adding default values to get theme

--- src/native/utils/helpers/getByTypographyVariant.ts
+++ src/native/utils/helpers/getByTypographyVariant.ts
@@ -1,9 +1,12 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { getTheme } from '@platformbuilders/helpers';
 import { scaledFontSize } from './fontScale';
 
-export const getFontSize = (props: any): any =>
-  scaledFontSize(getTheme(`${props.variant}.fontSize`)(props));
+export const getFontSize = (props: any): number => {
+  const fontSize = getTheme(`${props.variant}.fontSize`)(props) || 16;
+  return scaledFontSize(fontSize);
+};
 
-export const getLineHeight = (props: any): any =>
-  scaledFontSize(getTheme(`${props.variant}.lineHeight`)(props));
+export const getLineHeight = (props: any): number => {
+  const lineHeight = getTheme(`${props.variant}.lineHeight`)(props) || 22;
+  return scaledFontSize(lineHeight);
+};
