1cf25649bf815495547ac256c00b9c9ce1a69f57
Emiliano Oliveira <emilianom.oliveira.n@gmail.com>
2022-12-07 19:40:32 -0300

    Implements the button atom.

---
+++ src/atoms/button/Button.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  height: 48px;
+  padding: 0 16px;
+  background-color: #305bc4;
+  border: none;
+  border-radius: 4px;
+  color: #ffffff;
+  font-size: 15px;
+  font-weight: 700;
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
+`;
