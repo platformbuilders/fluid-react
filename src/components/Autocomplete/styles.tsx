294ed761c94e745a194490388b3cd6f7cff7a4cc
Will <willian.rodrigues.machado@hotmail.com>
2023-02-08 04:29:20 -0300

    cleanup autocomplete

--- src/components/Autocomplete/styles.tsx
+++ src/components/Autocomplete/styles.tsx
@@ -1,36 +1,3 @@
 import styled from 'styled-components';
-import { Checkbox, FormControlLabel } from '@material-ui/core';
-import { getTheme } from '../../utils/helpers';
 
-const brandPrimaryMain = getTheme('brand.primary.main');
-const successMain = getTheme('success.main');
-
-export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  margin-bottom: 10px;
-`;
-
-const ColoredCheckbox = styled((props) => (
-  <Checkbox
-    color="default"
-    checked={props.checked}
-    onChange={props.onChange}
-    {...props}
-  />
-))`
-  color: ${brandPrimaryMain};
-  & .Mui-checked {
-    color: ${successMain};
-  }
-`;
-
-export const StyledCheckbox = styled((props) => (
-  <FormControlLabel
-    control={
-      <ColoredCheckbox checked={props.checked} onChange={props.onChange} />
-    }
-    label={props.label}
-  />
-))``;
+export const Wrapper = styled.div``;
