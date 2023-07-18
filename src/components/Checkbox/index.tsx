512f8b3c500aee12722227ef2a8224285edaba48
Felipe Carvalho <felipecarvalho180@gmail.com>
2023-07-18 09:08:29 -0300

    feat(checkbox): created new checkbox component

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,6 +1,8 @@
-import { FC } from 'react';
+import { CSSProperties, FC } from 'react';
+
 import FormError from '../FormError';
-import { StyledCheckbox, Wrapper } from './styles';
+
+import { Check, CheckboxRoot, Label, Wrapper } from './styles';
 
 export type CheckboxProps = {
   onChange?(value: any): void;
@@ -8,12 +10,31 @@ export type CheckboxProps = {
   label?: string;
   checked?: boolean;
   name?: string;
+  id?: string;
+  style?: CSSProperties;
 };
 
-const Checkbox: FC<CheckboxProps> = ({ error }): JSX.Element => (
+const Checkbox: FC<CheckboxProps> = ({
+  id,
+  checked,
+  onChange,
+  label,
+  name,
+  error,
+  style,
+}): JSX.Element => (
   <Wrapper>
     <FormError error={error}>
-      <StyledCheckbox />
+      <CheckboxRoot htmlFor={id} style={style}>
+        <Check
+          type="checkbox"
+          id={id}
+          checked={checked}
+          onChange={onChange}
+          name={name}
+        />
+        <Label>{label}</Label>
+      </CheckboxRoot>
     </FormError>
   </Wrapper>
 );
--- src/components/Checkbox/styles.tsx
+++ src/components/Checkbox/styles.tsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import { getTheme } from '@platformbuilders/theme-toolkit';
+
+const brandPrimaryMain = getTheme('brand.primary.main');
+const brandPrimaryContrast = getTheme('brand.primary.contrast');
 
 export const Wrapper = styled.div`
   display: flex;
@@ -7,9 +11,136 @@ export const Wrapper = styled.div`
   margin-bottom: 10px;
 `;
 
-export const StyledCheckbox = styled(() => (
-  <label>
-    <input type="checkbox" />
-    <span>Checkbox</span>
-  </label>
-))``;
+export const CheckboxRoot = styled.label`
+  z-index: 0;
+  position: relative;
+  display: inline-block;
+  color: rgba(0, 0, 0, 0.87);
+  font-size: 16px;
+  line-height: 1.5;
+
+  input:checked + span::before,
+  input:indeterminate + span::before {
+    border-color: ${brandPrimaryMain};
+    background-color: ${brandPrimaryMain};
+  }
+
+  input:checked + span::after,
+  input:indeterminate + span::after {
+    border-color: ${brandPrimaryContrast};
+  }
+
+  input:indeterminate + span::after {
+    border-left: none;
+    transform: translate(4px, 3px);
+  }
+
+  &:hover {
+    input {
+      opacity: 0.12;
+
+      :focus {
+        opacity: 0.16;
+      }
+    }
+  }
+
+  input:active + span::before {
+    border-color: ${brandPrimaryMain};
+  }
+
+  input:checked:active + span::before {
+    border-color: transparent;
+    background-color: rgba(0, 0, 0, 0.6);
+  }
+
+  input:disabled + span {
+    color: rgba(0, 0, 0, 0.38);
+    cursor: initial;
+  }
+
+  input:disabled + span::before {
+    border-color: currentColor;
+  }
+
+  input:checked:disabled + span::before,
+  input:indeterminate:disabled + span::before {
+    border-color: transparent;
+    background-color: currentColor;
+  }
+`;
+
+export const Check = styled.input`
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  z-index: -1;
+  position: absolute;
+  left: -10px;
+  top: -8px;
+  display: block;
+  margin: 0;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  background-color: rgba(0, 0, 0, 0.6);
+  box-shadow: none;
+  outline: none;
+  opacity: 0;
+  transform: scale(1);
+  pointer-events: none;
+  transition: opacity 0.3s, transform 0.2s;
+
+  &:checked,
+  &:indeterminate {
+    background-color: ${brandPrimaryMain};
+  }
+
+  &:focus {
+    opacity: 0.04;
+  }
+
+  &:active {
+    opacity: 1;
+    transform: scale(0);
+    transition: transform 0s, opacity 0s;
+  }
+
+  &:disabled {
+    opacity: 0;
+  }
+`;
+
+export const Label = styled.span`
+  display: inline-block;
+  width: 100%;
+  cursor: pointer;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    box-sizing: border-box;
+    margin: 3px 11px 3px 1px;
+    border: solid 2px; /* Safari */
+    border-color: rgba(0, 0, 0, 0.6);
+    border-radius: 2px;
+    width: 18px;
+    height: 18px;
+    vertical-align: top;
+    transition: border-color 0.2s, background-color 0.2s;
+  }
+
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 3px;
+    left: 1px;
+    width: 10px;
+    height: 5px;
+    border: solid 2px transparent;
+    border-right: none;
+    border-top: none;
+    transform: translate(3px, 4px) rotate(-45deg);
+  }
+`;
