c49fa2c046ac685201edc9541c70d51ae4b12831
luancurti <luancurti@gmail.com>
2023-07-18 15:56:40 -0300

    refactor(loading): use lottie-web

--- src/components/LoadingIndicator/index.tsx
+++ src/components/LoadingIndicator/index.tsx
@@ -1,6 +1,64 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import lottie from 'lottie-web';
+import { AnimationObject } from '@platformbuilders/theme-toolkit';
+import animations from '../../theme/animations';
 import { LoadingType } from '../../types';
 
-const Loading: FC<LoadingType> = () => null;
+const loadingVariant = ({
+  variant,
+  contrast,
+}): undefined | string | number | AnimationObject | { uri: string } | null => {
+  switch (variant) {
+    case 'button':
+      return contrast
+        ? animations.contrastButtonLoading
+        : animations.buttonLoading;
+    case 'linear':
+      return contrast
+        ? animations.contrastLinearLoading
+        : animations.linearLoading;
+    default:
+      return contrast
+        ? animations.contrastCircularLoading
+        : animations.circularLoading;
+  }
+};
+
+const smallSize = {
+  width: 60,
+  height: 60,
+};
+
+const largeSize = {
+  width: 120,
+  height: 120,
+};
+
+const Loading: FC<LoadingType> = ({
+  large = false,
+  contrast = false,
+  variant = 'circular',
+  accessibility,
+}) => {
+  useEffect(() => {
+    const animationData = loadingVariant({ variant, contrast });
+
+    lottie.loadAnimation({
+      container: document.getElementById('loading') as Element,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData,
+    });
+  }, []);
+
+  return (
+    <div
+      style={large ? largeSize : smallSize}
+      id="loading"
+      aria-label={accessibility || 'Aguarde...'}
+    />
+  );
+};
 
 export default Loading;
