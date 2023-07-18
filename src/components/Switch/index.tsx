22c0600f8ef2a9c2f05183157c8a1ffd49389ab7
luancurti <luancurti@gmail.com>
2023-07-18 17:29:11 -0300

    feat(switch): add switch without material styles (#145)

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,5 +1,26 @@
-import styled from 'styled-components';
-import { SwitchProps } from '../../types';
-// import { getBackgroundColor, getHoverColor } from '../../utils';
+import { FC, useState } from 'react';
+import { Input, Label, Wrapper } from './styles';
 
-export default styled(({ label }) => <label>{label}</label>)<SwitchProps>``;
+type Props = {
+  id: string;
+  label: string;
+};
+
+const Switch: FC<Props> = ({ id, label }) => {
+  const [checked, setChecked] = useState(false);
+
+  const handleChangeChecked = () => {
+    setChecked((prevState) => !prevState);
+  };
+
+  return (
+    <Wrapper>
+      <Input type="checkbox" id={id} onChange={handleChangeChecked} />
+      <Label htmlFor={id} checked={checked}>
+        {label}
+      </Label>
+    </Wrapper>
+  );
+};
+
+export default Switch;
