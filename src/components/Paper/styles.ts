356caf22e626a4644258a1c918bf2d843dcd503e
Felipe Carvalho <37779345+felipecarvalho180@users.noreply.github.com>
2023-07-19 14:12:59 -0300

    Refactor/paper (#147)
    * refactor(paper): created new paper component
    
    * refactor(paper): typing changes

--- src/components/Paper/styles.ts
+++ src/components/Paper/styles.ts
@@ -1,6 +1,19 @@
+import { BoxShadowVariants } from 'src/enums';
 import styled from 'styled-components';
 import { getTheme } from '@platformbuilders/theme-toolkit';
 
-export const Wrapper = styled.div`
-  padding: ${getTheme('spacing.md')}px;
+const cardBorderRadius = getTheme('borderRadius.md');
+const spacingMd = getTheme('spacing.md');
+
+export type ElevationVariants = keyof typeof BoxShadowVariants;
+
+interface WrapperProps {
+  elevation?: ElevationVariants;
+  square?: boolean;
+}
+
+export const Wrapper = styled.div<WrapperProps>`
+  padding: ${spacingMd}px;
+  box-shadow: ${({ elevation }) => getTheme(`boxShadow.${elevation}`)};
+  border-radius: ${({ square }) => (square ? 0 : cardBorderRadius)}px;
 `;
