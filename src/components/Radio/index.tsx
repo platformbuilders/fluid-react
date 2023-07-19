1631196d4293f0b4ba2fd76ad195231c693b68bc
luancurti <luancurti@gmail.com>
2023-07-19 12:10:16 -0300

    feat(radio): add radio without material ui styles

--- src/components/Radio/index.tsx
+++ src/components/Radio/index.tsx
@@ -1,4 +1,19 @@
-import styled from 'styled-components';
-import { RadioProps } from '../../types';
+import { ChangeEvent, FC } from 'react';
 
-export default styled(({ label }) => <label>{label}</label>)<RadioProps>``;
+import { Input, Label, Wrapper } from './styles';
+
+export type Props = {
+  id: string;
+  label: string;
+  group: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Radio: FC<Props> = ({ onChange, id, label, group }) => (
+  <Wrapper>
+    <Input onChange={onChange} id={id} name={group} type="radio" />
+    <Label htmlFor={id}>{label}</Label>
+  </Wrapper>
+);
+
+export default Radio;
