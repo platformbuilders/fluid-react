384610ea84e74eb57dbea208d8d521b9e715612c
luancurti <luancurti@gmail.com>
2023-07-21 14:58:13 -0300

    refactor(text input): remove material ui styles (#150)

--- src/types/TextInput.ts
+++ src/types/TextInput.ts
@@ -6,13 +6,12 @@ export type TextInputType = {
   mask?: string;
   maskType?: string;
   formatChars?: { [key: string]: string };
-  label?: string;
+  label: string;
   error?: string | boolean;
-  placeholder?: string;
   fullWidth?: boolean;
   name: string;
   id: string;
-  type: string;
+  type?: string;
   maxlength?: string;
   pattern?: string;
   value: string | number | string[] | undefined;
