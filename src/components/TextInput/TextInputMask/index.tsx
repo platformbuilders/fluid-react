033db14665409bd19a152efc5ea9ba59edcc97df
luancurti <luancurti@gmail.com>
2023-07-25 16:34:04 -0300

    refactor(build): remove rollup

--- src/components/TextInput/TextInputMask/index.tsx
+++ src/components/TextInput/TextInputMask/index.tsx
@@ -1,17 +1,7 @@
 import { FC } from 'react';
-import ReactInputMask, { Props } from 'react-input-mask';
 import { TextInputType } from '../../../types';
 
 import { Input, Message, PlaceholderLabel } from '../styles';
-
-enum Mask {
-  cep = '99999-999',
-  cpf = '999.999.999-99',
-  cnpj = '99.999.999/9999-99',
-  birthdate = '99/99/9999',
-  phone = '(99) 9999-9999',
-  cellphone = '(99) 99999-9999',
-}
 
 const TextInputMask: FC<
   Partial<
@@ -34,19 +24,11 @@ const TextInputMask: FC<
   label,
   ...rest
 }) => (
-  <ReactInputMask
-    mask={(Mask[maskType as keyof typeof Mask] || mask) as Props['mask']}
-    onChange={onChange}
-    onBlur={onBlur}
-    onFocus={onFocus}
-    {...rest}
-  >
-    <>
-      <Input style={style} />
-      <PlaceholderLabel $hasValue={hasValue}>{label}</PlaceholderLabel>
-      {hasMessage ? <Message>{error}</Message> : null}
-    </>
-  </ReactInputMask>
+  <>
+    <Input {...rest} style={style} />
+    <PlaceholderLabel $hasValue={hasValue}>{label}</PlaceholderLabel>
+    {hasMessage ? <Message>{error}</Message> : null}
+  </>
 );
 
 export default TextInputMask;
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,10 +2,6 @@ export * from './Button';
 
 export * from './TextInput';
 export * from './Loading';
-export * from './Switch';
-export * from './Radio';
-export * from './Switch';
-export * from './LazyWrapper';
 export * from './Typography';
 export * from './TouchableType';
 export * from './Select';
