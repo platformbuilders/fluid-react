8ac6b51466ed22d09ee0b2349952ab72cea6f60e
marcelxsilva <maarcelomonteiro@gmail.com>
2024-02-21 12:09:57 -0300

    feat: touchable

---
+++ src/components/Touchable/touchable.stories.tsx
@@ -0,0 +1,65 @@
+import { expect, jest } from '@storybook/jest';
+import type { Meta, StoryObj } from '@storybook/react';
+import { configure, userEvent, within } from '@storybook/testing-library';
+
+import Touchable from './index';
+
+configure({ testIdAttribute: 'testid' });
+
+//  Mocks
+const mockTextId = 'touchable-test-id';
+const mockDisabledTextId = 'touchable-disabled-test-id';
+const mockOnPress = jest.fn();
+
+const meta: Meta<typeof Touchable> = {
+  title: 'Components/Touchable',
+  component: Touchable,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {
+    testID: mockTextId,
+    children: 'Touchable Test',
+    onPress: mockOnPress,
+  },
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    await step('Touchable | Test Render', async () => {
+      expect(canvas.getByTestId(mockTextId)).toBeInTheDocument();
+    });
+
+    await step('Touchable | Event Click', async () => {
+      await userEvent.click(canvas.getByTestId(mockTextId));
+      expect(mockOnPress).toHaveBeenCalled();
+    });
+  },
+};
+
+type Story = StoryObj<typeof Touchable>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    testID: mockDisabledTextId,
+  },
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    await step('Touchable | Test Render', async () => {
+      expect(canvas.getByTestId(mockDisabledTextId)).toBeInTheDocument();
+    });
+
+    await step('Touchable | Event Click', async () => {
+      await userEvent.click(canvas.getByTestId(mockDisabledTextId));
+      expect(mockOnPress).not.toHaveBeenCalled();
+    });
+  },
+};
+
+export default meta;
